edf4260d9479c3684b6c6e0599fa53c915c19cea
Andrew Alford <andrewalford1@outlook.com>
2024-10-12 14:02:51 +0100

    add about me page

--- src/app/slideshow/slideshow.component.ts
+++ src/app/slideshow/slideshow.component.ts
@@ -7,11 +7,17 @@ import { register } from 'swiper/element/bundle';
 import { TitleSlideComponent } from './slides/title-slide/title-slide.component';
 import { ContentsComponent } from './slides/contents/contents.component';
 import { CustomThemeComponent } from './slides/custom-theme/custom-theme.component';
+import { AboutMeComponent } from './slides/about-me/about-me.component';
 
 @Component({
   selector: 'app-slideshow',
   standalone: true,
-  imports: [TitleSlideComponent, ContentsComponent, CustomThemeComponent],
+  imports: [
+    TitleSlideComponent,
+    ContentsComponent,
+    CustomThemeComponent,
+    AboutMeComponent,
+  ],
   templateUrl: './slideshow.component.html',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
