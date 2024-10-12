358e4dafab1da09f3e12db568a9240e81961df31
Andrew Alford <andrewalford1@outlook.com>
2024-10-12 15:50:01 +0100

    add pseudo classes component

---
+++ src/app/slideshow/slides/pseudo-classes/pseudo-classes.component.ts
@@ -0,0 +1,15 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-pseudo-classes',
+  standalone: true,
+  templateUrl: './pseudo-classes.component.html',
+})
+export class PseudoClassesComponent {
+  protected buttonExample = signal<string>(`
+    &lt;button class="rounded bg-red-600 px-2 py-1 text-2xl italic <span class="font-bold underline">hover:bg-red-500 hover:text-yellow-300</span>">
+        Click me
+    <&sol;button>`);
+
+  protected people = signal<string[]>(['Sarah', 'Joshua', 'Emily', 'Martin']);
+}
--- src/app/slideshow/slideshow.component.ts
+++ src/app/slideshow/slideshow.component.ts
@@ -8,6 +8,7 @@ import { TitleSlideComponent } from './slides/title-slide/title-slide.component'
 import { ContentsComponent } from './slides/contents/contents.component';
 import { CustomThemeComponent } from './slides/custom-theme/custom-theme.component';
 import { AboutMeComponent } from './slides/about-me/about-me.component';
+import { PseudoClassesComponent } from './slides/pseudo-classes/pseudo-classes.component';
 
 @Component({
   selector: 'app-slideshow',
@@ -17,6 +18,7 @@ import { AboutMeComponent } from './slides/about-me/about-me.component';
     ContentsComponent,
     CustomThemeComponent,
     AboutMeComponent,
+    PseudoClassesComponent,
   ],
   templateUrl: './slideshow.component.html',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
