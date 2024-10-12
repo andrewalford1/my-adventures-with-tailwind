4316d3ae924f225e7a959c77962ac6fe26090f7f
Andrew Alford <andrewalford1@outlook.com>
2024-10-12 09:53:48 +0100

    nest slides under slideshow

--- src/app/slideshow/slideshow.component.ts
+++ src/app/slideshow/slideshow.component.ts
@@ -4,9 +4,9 @@ import {
   CUSTOM_ELEMENTS_SCHEMA,
 } from '@angular/core';
 import { register } from 'swiper/element/bundle';
-import { TitleSlideComponent } from '../title-slide/title-slide.component';
-import { ContentsComponent } from '../contents/contents.component';
-import { CustomThemeComponent } from '../custom-theme/custom-theme.component';
+import { TitleSlideComponent } from './slides/title-slide/title-slide.component';
+import { ContentsComponent } from './slides/contents/contents.component';
+import { CustomThemeComponent } from './slides/custom-theme/custom-theme.component';
 
 @Component({
   selector: 'app-slideshow',
